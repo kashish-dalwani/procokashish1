d2c0b24a26f1fde1b5bd249fc38cccf3f84b4d50
kashish-dalwani <dalwanikashish25@gmail.com>
2024-09-27 07:26:02 +0530

    removed navbar and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         
         {children}
-        <footer>bye</footer>
+        {/* <footer>bye</footer> */}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Section1 from '../components/Section1';
-import Navbar from '../components/Navbar.jsx';
+// import Navbar from '../components/Navbar.jsx';
 import Section2 from "../components/Section2";
 import Section3 from "../components/Section3";
 import Section4 from "../components/Section4";
@@ -9,7 +9,7 @@ import Section4 from "../components/Section4";
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <Section1 />
       <Section2 />
       <Section3 />
